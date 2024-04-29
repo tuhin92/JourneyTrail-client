1d2cc7a448a7b9ee064145a987e278e4dc2a3ab8
tuhin92 <tuhinalmamun71m@gmail.com>
2024-04-29 11:51:32 +0600

    little bit change slider

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -3,7 +3,7 @@ const Slider = () => {
         <div className="mt-6">
             <div className="carousel w-full h-3/2">
   <div id="slide1" className="carousel-item relative w-full">
-    <img src="https://i.ibb.co/DfCpX2K/ravin-rau-otb0-Wq-G3l34-unsplash.jpg" className="w-full h-[60%] rounded-2xl" />
+    <img src="https://i.ibb.co/xs3t362/s-m-ibrahim-1-NEJki-TTu-LU-unsplash.jpg" className="w-full h-[60%] rounded-2xl" />
     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/3">
       <a href="#slide4" className="btn btn-circle">❮</a> 
       <a href="#slide2" className="btn btn-circle">❯</a>
